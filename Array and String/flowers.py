e3e23ff71a9957a4ada6929543e88702edb8ad8d
kamil-1929 <kamil.329@outlook.com>
2025-03-21 22:02:48 +0000

    reverse

--- Array and String/flowers.py
+++ Array and String/flowers.py
@@ -23,3 +23,22 @@
 # flowerbed[i] is 0 or 1.
 # There are no two adjacent flowers in flowerbed.
 # 0 <= n <= flowerbed.length
+
+class FlowerBed:
+    def canPlaceFlowers(self, flowerbed, n):
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                empty_left = (i==0) or (flowerbed[i-1] == 0)
+                empty_right = (i == len(flowerbed)-1) or (flowerbed[i + 1] == 0)
+                if empty_left and empty_right:
+                    flowerbed[i] = 1
+                    n -=1
+                    if n == 0:
+                        return True
+        return n <= 0 
+    
+plant = FlowerBed()
+flowerbed = [1,0,0,0,1] 
+n = 2
+result = plant.canPlaceFlowers(flowerbed, n)
+print(result)
---
+++ Array and String/reversestring.py
@@ -0,0 +1,48 @@
+# Given a string s, reverse only all the vowels in the string and return it.
+
+# The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+# Example 1:
+# Input: s = "IceCreAm"
+# Output: "AceCreIm"
+
+# Explanation:
+# The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+# Example 2:
+# Input: s = "leetcode"
+# Output: "leotcede"
+
+ 
+# Constraints:
+
+# 1 <= s.length <= 3 * 105
+# s consist of printable ASCII characters.
+
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        vowels = ['a','e','i','o','u', 'A','E','I', 'O', 'U']
+        l = 0
+        r = len(s) -1
+        s = list(s)
+           
+        while l < r:
+            if s[l] not in vowels:
+                l += 1
+                
+            if s[r] not in vowels:
+                r -= 1
+            
+            if s[l] in vowels and s[r] in vowels:
+                s[l], s[r] = s[r], s[l]
+                
+                l += 1
+                r -= 1
+        
+        return ''.join(s)
+        
+            
+            
+            
